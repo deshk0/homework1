ff9ccee3b48b733ac5f0fc75a0710e41f718d1f7
deshk0 <50766489+deshk0@users.noreply.github.com>
2019-07-13 00:17:34 +0300

    Add files via upload

---
+++ lesson 17/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+// 1
+
+let h = 3;
+
+let m = 60;
+
+let s = 60;
+
+let result = h * m * s ;
+
+console.log(`В ${h} часах: ${result} секунд`)
+
+// 2 
+
+let num = 1;
+
+num += 12;
+
+num -= 14;
+
+num *= 5;
+
+num /= 7;
+
+num += 1;
+
+num -= 1;
+
+alert(num);
+
+// 3 
+
+let a = 10;
+let b = 2;
+let c = 5;
+
+console.log( a + b + c );
+
+// 4 
+
+a = 17;
+b = 10;
+
+c = a - b;
+
+let d = 7;
+
+result = c + d;
+
+console.log( result );
+
+// 5 
+
+let name = 'Batman';
+
+console.log( 'Привет' + ' ' + name + '!');
